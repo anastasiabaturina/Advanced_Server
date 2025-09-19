ffea711acaba2a2374b85a973a12332575f36881
Anastasia Baturina <a.baturina@sh.dunice.net>
2025-09-19 09:15:13 +0300

    The login test was completed

--- Server_Ad_Baturina.IntegrationTest/Api/AuthControllerIntegrationTests.cs
+++ Server_Ad_Baturina.IntegrationTest/Api/AuthControllerIntegrationTests.cs
@@ -22,11 +22,11 @@ public class AuthControllerIntegrationTests : IClassFixture<TestingWebAppFactory
     {
         // Arrange
         var registerRequest = _fixture.Build<RegisterUserRequest>()
-                .With(r => r.Name, _fixture.Create<string>().Substring(0, 25))
-                .With(r => r.Role, _fixture.Create<string>().Substring(0, 25))
-                .With(r => r.Email, "testuser@example.com")
-                .With(r => r.Password, "StrongPassword123!")
-                .Create();
+             .With(r => r.Name, _fixture.Create<string>().Substring(0, 25))
+             .With(r => r.Role, _fixture.Create<string>().Substring(0, 25))
+             .With(r => r.Email, "testuser@example.com")
+             .With(r => r.Password, "StrongPassword123!")
+             .Create();
 
         // Act
         var response = await _client.PostAsJsonAsync("/api/v1/auth/register", registerRequest);
@@ -40,5 +40,42 @@ public class AuthControllerIntegrationTests : IClassFixture<TestingWebAppFactory
         Assert.NotNull(responseData.Data);
         Assert.False(string.IsNullOrEmpty(responseData.Data.Token));
         Assert.Equal(registerRequest.Email, responseData.Data.Email);
+    }
+
+    [Fact]
+    public async Task Login_POST_ReturnsOkResponse()
+    {
+        //Arrange
+        var password = "StrongPassword123dswdxs!";
+        var email = "testuser@examplvcfve.com";
+
+        var registerRequest = _fixture.Build<RegisterUserRequest>()
+            .With(x => x.Email, email)
+            .With(x => x.Password, password)
+            .With(r => r.Name, _fixture.Create<string>().Substring(0, 25))
+            .With(r => r.Role, _fixture.Create<string>().Substring(0, 25))
+            .Create();
+
+        var registerResponse = await _client.PostAsJsonAsync("/api/v1/auth/register", registerRequest);
+
+        registerResponse.EnsureSuccessStatusCode(); 
+
+        // Act
+        var loginRequest = new SignInRequest
+        {
+            Email = email,
+            Password = password
+        };
+
+        var loginResponse = await _client.PostAsJsonAsync("/api/v1/auth/sign-in", loginRequest);
+        var responseBody = await loginResponse.Content.ReadFromJsonAsync<CustomSuccessResponse<SignInUserResponse>>();
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, loginResponse.StatusCode);       
+        Assert.NotNull(responseBody);
+        Assert.True(responseBody.Success);
+        Assert.NotNull(responseBody.Data);
+        Assert.False(string.IsNullOrEmpty(responseBody.Data.Token));
+        Assert.Equal(email, responseBody.Data.Email);
     }
 }
